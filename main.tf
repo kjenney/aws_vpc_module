fa9e87ad6e71360251c357ecfc3cddbd3fe0293a
Ken Jenney <kjenney@gmail.com>
2023-12-12 18:11:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,27 @@
-variable "ipam_pool_description" {}
-variable "transit_gateway_description" {}
+resource "aws_vpc" "my_vpc" {
+  ipv4_ipam_pool_id   = data.aws_vpc_ipam_pool.my_pool.id
+  cidr_block          = data.aws_vpc_ipam_preview_next_cidr.previewed_cidr.cidr
+
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "My-VPC"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count                 = length(local.private_subnets)
+  availability_zone     = length(regexall("^[a-z]{2}-", element(local.azs, count.index))) > 0 ? element(local.azs, count.index) : null
+  cidr_block            = local.private_subnets[count.index]
+  vpc_id                = aws_vpc.my_vpc.id
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "my_tgw_attachment" {
+  subnet_ids         = aws_subnet.private_subnets[*].id
+  transit_gateway_id = data.aws_ec2_transit_gateway.my_tgw.id
+  vpc_id             = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "My-VPC-TGW-Attachment"
+  }
+}
