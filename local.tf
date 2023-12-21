02b68424f3ff9228d9ce1b7a72be829d3526cc2d
Ken Jenney <kjenney@gmail.com>
2023-12-21 11:01:59 -0500

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,12 +1,13 @@
 locals {
   cidr              = aws_vpc.my_vpc.cidr_block
-  zones             = 2
-  private_newbits   = 2
+  zones             = 4
+  ipv4_step           = length(local.azs) > 3 ? 2 : 1 # The more subnets we have the more room we'll need for our IP space
+  private_newbits   = 3
   public_newbits    = 3
   public_netnum_add = 4
   azs               = slice(coalesce(var.azs, data.aws_availability_zones.available.names), 0, local.zones)
 
   # IPv4 subnets
-  private_subnets   = [for k, v in local.azs : cidrsubnet(local.cidr, local.private_newbits, k)]
-  public_subnets    = [for k, v in local.azs : cidrsubnet(local.cidr, local.public_newbits, k + local.public_netnum_add)]
+  private_subnets     = [for k, v in local.azs : cidrsubnet(var.cidr, local.private_newbits, k)]
+  public_subnet      = cidrsubnet(var.cidr, local.public_newbits, local.ipv4_step + length(local.azs))
 }
